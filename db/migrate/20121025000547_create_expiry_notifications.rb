e1c791e80abba133e8418f8f72ff790aaf72375e
sheenobu <sheena.artrip@gmail.com>
2012-10-26 10:43:38 -0400

    Fixed typos.

--- db/migrate/20121025000547_create_expiry_notifications.rb
+++ db/migrate/20121025000547_create_expiry_notifications.rb
@@ -2,7 +2,7 @@ class CreateExpiryNotifications < ActiveRecord::Migration
   def up
     create_table :spree_expiry_notifications do |t|
       t.references :subscription
-      t.interer :interval
+      t.integer :interval
       t.timestamps
     end
   end
